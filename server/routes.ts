524f9f67012afa82b67d62e7f1eed3e2e042f5d1
caspted <teddiejohn.rajeev-22@cpu.edu.ph>
2024-03-11 11:36:26 +0800

    added routes for CRUD

--- server/routes.ts
+++ server/routes.ts
@@ -7,12 +7,56 @@ function routes(app: Express) {
   app.get("/", (req: Request, res: Response) => {
     res.send("This is a simple server!");
   });
-  
+
   app.get("/api/pogs", async (req: Request, res: Response) => {
     const pogs = await prisma.pogs.findMany();
-  
-    res.json(pogs); 
-  })
+
+    res.json(pogs);
+  });
+
+  app.get("/api/pogs:id", async (req: Request, res: Response) => {
+    const pogs = await prisma.pogs.findUnique({
+      where: {
+        id: 1,
+      },
+    });
+    res.json(pogs);
+  });
+
+  app.post("/api/pogs", async (req: Request, res: Response) => {
+    const pogs = await prisma.pogs.create({
+      data: {
+        name: "Tepig",
+        ticker_symbol: "TPIG",
+        price: 75,
+        color: "orange",
+      },
+    });
+    res.json(pogs);
+  });
+
+  app.put("/api/pogs:id", async (req: Request, res: Response) => {
+    const pogs = await prisma.pogs.update({
+      where: {
+        id: 1,
+      },
+      data: {
+        name: "Teboar",
+        ticker_symbol: "TBOAR",
+        price: 100,
+        color: "blue",
+      },
+    });
+    res.json(pogs);
+  });
+
+  app.delete("/api/pogs:id", async (req: Request, res: Response) => {
+    const pogs = await prisma.pogs.delete({
+      where: {
+        id: 2,
+      },
+    });
+  });
 }
 
 export default routes;
