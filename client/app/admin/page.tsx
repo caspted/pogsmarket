863816f54a3603f362eb7768f763b4261b45d433
bit-Matt <ledesma.matthew.b@gmail.com>
2024-04-28 20:05:23 +0800

    [Matt][Add] admin ui

--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -1,41 +1,44 @@
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow,} from "@/components/ui/table"
+"use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z.string().nonempty().min(2).max(100),
+  currentPrice: z.number().int().positive(),
+  color: z.string().nonempty(),
+  ticker: z.string().nonempty().toUpperCase().min(3).max(5),
+});
 
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      currentPrice: 0,
+      color: "",
+      ticker: "",
+    },
+  });
+
+  const handleSubmit = () => {};
+
   return (
     <main>
-      <div className="flex overflow-hidden space-x-4">
-        <div className="flex space-x-4 ml-2 animate-loop-scroll">
-          <Badge variant="secondary">token1</Badge>
-          <Badge variant="secondary">token2</Badge>
-          <Badge variant="secondary">token3</Badge>
-          <Badge variant="secondary">token4</Badge>
-          <Badge variant="secondary">token5</Badge>
-          <Badge variant="secondary">token6</Badge>
-          <Badge variant="secondary">token7</Badge>
-          <Badge variant="secondary">token8</Badge>
-          <Badge variant="secondary">token9</Badge>
-          <Badge variant="secondary">token10</Badge>
-          <Badge variant="secondary">token11</Badge>
-          <Badge variant="secondary">token12</Badge>
+      <div className="ml-4">
+        <h1 className="text-3xl font-bold mb-8">Admin Panel</h1>
+      </div>
+      <div>
+        <div>
+          <h2 className="text-xl font-bold mb-2 ml-4">Manage Pogs</h2>
         </div>
-        
-        {/* Duplicate the items to make it look like its looping */}
-        <div className="flex space-x-4 animate-loop-scroll" aria-hidden="true"> 
-          <Badge variant="secondary">token1</Badge>
-          <Badge variant="secondary">token2</Badge>
-          <Badge variant="secondary">token3</Badge>
-          <Badge variant="secondary">token4</Badge>
-          <Badge variant="secondary">token5</Badge>
-          <Badge variant="secondary">token6</Badge>
-          <Badge variant="secondary">token7</Badge>
-          <Badge variant="secondary">token8</Badge>
-          <Badge variant="secondary">token9</Badge>
-          <Badge variant="secondary">token10</Badge>
-          <Badge variant="secondary">token11</Badge>
-          <Badge variant="secondary">token12</Badge>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+
+            </form>
+          </Form>
         </div>
       </div>
     </main>
