b373e48415a52a8b6cc9f555be7a7264215234f9
caspted <teddiejohn.rajeev26@gmail.com>
2024-04-28 17:30:39 +0800

    fixed admin routes and tested it

--- server/routes.ts
+++ server/routes.ts
@@ -45,8 +45,24 @@ function routes(app: Express) {
 //admin side APIs
   app.get("/api/admin/pogs", async (req: Request, res: Response) => {
     try {
-      const adminPogs = prisma.pogs.findMany()
+      const adminPogs = await prisma.pogs.findMany()
       res.status(200).json(adminPogs)
+
+    } catch {
+      res.status(500).json({ error: 'Interal Server Error'})
+    }
+
+  })
+
+  app.get("/api/admin/pogs/:id", async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params
+      const adminPog = await prisma.pogs.findUnique({
+        where: {
+          id: parseInt(id)
+        }
+      })
+      res.status(200).json(adminPog)
 
     } catch {
       res.status(500).json({ error: 'Interal Server Error'})
@@ -56,10 +72,9 @@ function routes(app: Express) {
 
   app.post("/api/admin/pogs", async (req: Request, res: Response) => {
     try {
-      const { name, ticker_symbol, current_price, color} = req.body
-      const previous_price = current_price
+      const { name, ticker_symbol, previous_price, current_price, color} = req.body
   
-      const newPogs = prisma.pogs.create({
+      const newPogs = await prisma.pogs.create({
         data: {
           name,
           ticker_symbol,
@@ -80,7 +95,7 @@ function routes(app: Express) {
   app.put("/api/admin/pogs/:id", async (req: Request, res: Response) => {
     try {
       const { id } = req.params;
-      const { name, ticker_symbol, current_price, color } = req.body;
+      const { name, ticker_symbol, previous_price, current_price, color } = req.body;
     
       const updatedPog = await prisma.pogs.update({
         where: {
@@ -89,6 +104,7 @@ function routes(app: Express) {
         data: {
           name,
           ticker_symbol,
+          previous_price,
           current_price,
           color
         }
